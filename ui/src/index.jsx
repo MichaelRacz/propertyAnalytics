30fb1c83ea56e9331c4a6a2abcd75f624e6d468e
Michael Racz <mr.michael.racz@gmail.com>
2017-02-13 20:38:58 +0100

    implement a static server to host the ui app

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -5,35 +5,32 @@ var properties = [{description: "a", rent: 600, squareMetres: 65},
   {description: "b", rent: 700, squareMetres: 75},
   {description: "c", rent: 800, squareMetres: 85}];
 
-// TODO: class syntax
-var Property = React.createClass({
-  render: function() {
-    return (
-      <section>
+class Property extends React.Component {
+  render() {
+    return (<section>
         {this.props.property.description}
         {this.props.property.rent}
         {this.props.property.squareMetres}
-      </section>
-    );
-  },
-});
+      </section>);
+  }
+};
 
-var PropertyList = React.createClass({
-  render: function() {
+class PropertyList extends React.Component {
+  render() {
     var properties = this.props.properties.map(function(property) {
       return React.createElement(Property, { property: property });
     });
                   
-    return (
-      <div>
+    return (<div>
         <h1>Properties</h1>
         <p>This is a list of available properties</p>
         {properties}
-      </div>
-    );
-  },
+      </div>);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+  var container = document.querySelector("#container");
+  ReactDOM.render(<PropertyList properties={properties}></PropertyList>, container);
 });
 
-var mainElement = document.querySelector("#container");
-
-ReactDOM.render(<PropertyList properties={properties}></PropertyList>, mainElement);
